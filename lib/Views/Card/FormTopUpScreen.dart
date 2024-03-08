ef88ac59749c452c053f07e4243c98fe95e2f1c1
dannyjiustian <danny.djie72@gmail.com>
2024-03-08 18:12:27 +0700

    Add new screen for the top up the saldo

---
+++ lib/Views/Card/FormTopUpScreen.dart
@@ -0,0 +1,128 @@
+import 'package:app_pembayaran/Views/Card/SuccessTopUpScreen.dart';
+import 'package:app_pembayaran/Views/Widget/CardRFIDVirtualWidget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:iconsax/iconsax.dart';
+
+import '../Widget/ButtonWidget.dart';
+import '../Widget/IconAppbarCostuimeWidget.dart';
+import '../Widget/TextFieldInputWidget.dart';
+
+class FormTopUpScreen extends StatefulWidget {
+  const FormTopUpScreen({super.key});
+
+  @override
+  State<FormTopUpScreen> createState() => _FormTopUpScreenState();
+}
+
+class _FormTopUpScreenState extends State<FormTopUpScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  final amount = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    final mediaQueryHeight = MediaQuery.of(context).size.height;
+    final mediaQueryWidth = MediaQuery.of(context).size.width;
+
+    final appBar = PreferredSize(
+      preferredSize: Size.zero,
+      child: AppBar(
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          // Status bar color
+          statusBarColor: Colors.transparent,
+
+          // Status bar brightness (optional)
+          statusBarIconBrightness: Brightness.dark, // For Android (dark icons)
+          statusBarBrightness: Brightness.light, // For iOS (dark icons)
+        ),
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+      ),
+    );
+
+    final bodyHeight = mediaQueryHeight -
+        appBar.preferredSize.height -
+        MediaQuery.of(context).padding.top;
+    return MaterialApp(
+      title: "Form TopUp",
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+      home: Scaffold(
+        appBar: appBar,
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: SingleChildScrollView(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  IconAppbarCustomWidget(
+                    iconType: Iconsax.arrow_left_2,
+                    functionTap: () async {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Text(
+                      "Kembali",
+                      style: GoogleFonts.poppins(
+                          fontSize: 14, fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                  height: 180,
+                  child:
+                      CardRFIDVirtualWidget(mediaQueryWidth: mediaQueryWidth)),
+              SizedBox(
+                height: 30,
+              ),
+              Form(
+                key: _formKey,
+                child: Container(
+                  height: bodyHeight * 0.4,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      TextFieldInputWidget(
+                        nameController: amount,
+                        label: "Jumlah Pengisian",
+                        obscureCondition: false,
+                        keyboardNext: true,
+                        keyboard: false,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      ButtonWidget(
+                        buttonText: "Isi Kartu",
+                        colorSetBody: Colors.blue,
+                        colorSetText: Colors.white,
+                        functionTap: () async {
+                          FocusScope.of(context).unfocus();
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => SuccessTopUpScreen()));
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ]),
+          ),
+        ),
+      ),
+    );
+  }
+}
