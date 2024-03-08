28d99284ceb26bdfe70816fa255faa04f308b231
dannyjiustian <danny.djie72@gmail.com>
2024-03-08 17:02:18 +0700

    add new screen for the read card

---
+++ lib/Views/Card/DetectCardScreen.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:iconsax/iconsax.dart';
+
+import '../Widget/IconAppbarCostuimeWidget.dart';
+
+class DetectCardScreen extends StatefulWidget {
+  const DetectCardScreen({super.key});
+
+  @override
+  State<DetectCardScreen> createState() => _DetectCardScreenState();
+}
+
+class _DetectCardScreenState extends State<DetectCardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final mediaQueryHeight = MediaQuery.of(context).size.height;
+    final mediaQueryWidth = MediaQuery.of(context).size.width;
+
+    final appBar = PreferredSize(
+      preferredSize: Size.zero,
+      child: AppBar(
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          // Status bar color
+          statusBarColor: Colors.transparent,
+
+          // Status bar brightness (optional)
+          statusBarIconBrightness: Brightness.dark, // For Android (dark icons)
+          statusBarBrightness: Brightness.light, // For iOS (dark icons)
+        ),
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+      ),
+    );
+
+    final bodyHeight = mediaQueryHeight -
+        appBar.preferredSize.height -
+        MediaQuery.of(context).padding.top;
+    return MaterialApp(
+        title: "Top Up",
+        theme: ThemeData(
+          textTheme: GoogleFonts.poppinsTextTheme(
+            Theme.of(context).textTheme,
+          ),
+        ),
+        home: Scaffold(
+          appBar: appBar,
+          body: Padding(
+            padding: const EdgeInsets.all(20),
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  IconAppbarCustomWidget(
+                    iconType: Iconsax.arrow_left_2,
+                    functionTap: () async {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Text(
+                      "Kembali",
+                      style: GoogleFonts.poppins(
+                          fontSize: 14, fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    "assets/img/contactless.svg",
+                    width: 200,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "Tempelkan kartu Kamu ke area NFC Ponsel",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.poppins(
+                        fontSize: 18, fontWeight: FontWeight.w600),
+                  ),
+                ],
+              ))
+            ]),
+          ),
+        ));
+  }
+}
