4cf791cee1dcc11f87f1f02e141909835df08ad2
dannyjiustian <danny.djie72@gmail.com>
2024-03-08 18:39:53 +0700

    add new screen for success proccess the card

---
+++ lib/Views/Card/SuccessScreen.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../Home/Home/HomeScreen.dart';
+import '../Widget/ButtonWidget.dart';
+
+class SuccessScreen extends StatelessWidget {
+  const SuccessScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaQueryHeight = MediaQuery.of(context).size.height;
+    final mediaQueryWidth = MediaQuery.of(context).size.width;
+
+    final appBar = PreferredSize(
+      preferredSize: Size.zero,
+      child: AppBar(
+        systemOverlayStyle: SystemUiOverlayStyle(
+          // Status bar color
+          statusBarColor: Colors.transparent,
+
+          // Status bar brightness (optional)
+          statusBarIconBrightness: Brightness.dark, // For Android (dark icons)
+          statusBarBrightness: Brightness.light, // For iOS (dark icons)
+        ),
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+      ),
+    );
+
+    final bodyHeight = mediaQueryHeight -
+        appBar.preferredSize.height -
+        MediaQuery.of(context).padding.top;
+    return MaterialApp(
+      title: "Forgot",
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+      home: Scaffold(
+        appBar: appBar,
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 25, horizontal: 60),
+                  height: bodyHeight * 0.5,
+                  child: SvgPicture.asset("assets/img/success.svg"),
+                ),
+                Container(
+                    height: bodyHeight * 0.3,
+                    child: Column(
+                      children: [
+                        Text(
+                          "Selamat Telah Melakukan Top Up!",
+                          style: GoogleFonts.poppins(
+                              fontSize: 20, fontWeight: FontWeight.w600),
+                        ),
+                        Text(
+                          "Saldo berhasil ditambahkan keakunmu, silakan kembali ke home untuk mengeceknya!",
+                          style: GoogleFonts.poppins(
+                              fontSize: 14, color: Colors.grey.shade500),
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          "Untuk Mebilhat detail transaksi dapat ke menuu history.",
+                          style: GoogleFonts.poppins(
+                              fontSize: 14,
+                              color: Colors.grey.shade500,
+                              fontWeight: FontWeight.w600),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    )),
+                Container(
+                  height: bodyHeight * 0.2,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ButtonWidget(
+                        buttonText: "Kembali",
+                        colorSetBody: Colors.blue,
+                        colorSetText: Colors.white,
+                        functionTap: () {
+                          Navigator.of(context).pushAndRemoveUntil(
+                              MaterialPageRoute(
+                                  builder: (context) => HomeScreen()),
+                              (Route<dynamic> route) => false);
+                        },
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
