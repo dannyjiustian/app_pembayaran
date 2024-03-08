6d559983d2824eeaeadfed7c71b556dc210998fd
dannyjiustian <danny.djie72@gmail.com>
2024-03-08 18:51:18 +0700

    add new widget for card list transaction

---
+++ lib/Views/Widget/CardListTransactionWidget.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CardListTranasctionWidget extends StatelessWidget {
+  const CardListTranasctionWidget({
+    super.key,
+    required this.mediaQueryWidth,
+  });
+
+  final double mediaQueryWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: mediaQueryWidth,
+      margin: const EdgeInsets.symmetric(horizontal: 2.5),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(color: Colors.grey.shade300),
+        color: Colors.blue.shade100,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const CircleAvatar(
+                  backgroundImage: AssetImage("assets/img/logo.png"),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Pembayaran",
+                            style: GoogleFonts.poppins(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.black),
+                          ),
+                          Text(
+                            "01 Mar 2024",
+                            style: GoogleFonts.poppins(
+                                fontSize: 12, color: Colors.black),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            "-Rp.5.000",
+                            style: GoogleFonts.poppins(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.black),
+                          ),
+                          Text(
+                            "Uang Keluar",
+                            style: GoogleFonts.poppins(
+                                fontSize: 12, color: Colors.black),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
