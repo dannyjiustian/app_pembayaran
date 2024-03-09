28391870a5ba1610dcb41d9c14b38a5b8723a710
dannyjiustian <danny.djie72@gmail.com>
2024-03-09 14:09:25 +0700

    add new screen for the withdraw

---
+++ lib/Views/Withdraw/WithdrawScreen.dart
@@ -0,0 +1,172 @@
+import 'package:app_pembayaran/Views/Card/SuccessScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:iconsax/iconsax.dart';
+
+import '../Widget/ButtonWidget.dart';
+import '../Widget/IconAppbarCostuimeWidget.dart';
+import '../Widget/TextFieldInputWidget.dart';
+
+class WithdrawScreen extends StatefulWidget {
+  const WithdrawScreen({super.key});
+
+  @override
+  State<WithdrawScreen> createState() => _WithdrawScreenState();
+}
+
+class _WithdrawScreenState extends State<WithdrawScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  final amount = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    final mediaQueryHeight = MediaQuery.of(context).size.height;
+    final mediaQueryWidth = MediaQuery.of(context).size.width;
+
+    final appBar = PreferredSize(
+      preferredSize: Size.zero,
+      child: AppBar(
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          // Status bar color
+          statusBarColor: Colors.transparent,
+
+          // Status bar brightness (optional)
+          statusBarIconBrightness: Brightness.dark, // For Android (dark icons)
+          statusBarBrightness: Brightness.light, // For iOS (dark icons)
+        ),
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+      ),
+    );
+
+    final bodyHeight = mediaQueryHeight -
+        appBar.preferredSize.height -
+        MediaQuery.of(context).padding.top;
+    return MaterialApp(
+      title: "Daftar Kartu",
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+      home: Scaffold(
+        appBar: appBar,
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: SingleChildScrollView(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  IconAppbarCustomWidget(
+                    iconType: Iconsax.arrow_left_2,
+                    functionTap: () async {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Text(
+                      "Kembali",
+                      style: GoogleFonts.poppins(
+                          fontSize: 14, fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                height: 180,
+                child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 2.5),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.grey.shade300),
+                      color: Colors.blue.shade100),
+                  width: mediaQueryWidth,
+                  child: Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Total Pendapatan",
+                          style: GoogleFonts.poppins(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Saldo",
+                                    style: GoogleFonts.poppins(
+                                        fontSize: 10, color: Colors.black),
+                                  ),
+                                  Text(
+                                    "Rp. 10.000",
+                                    style: GoogleFonts.poppins(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w600,
+                                        color: Colors.black),
+                                  ),
+                                ]),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Form(
+                key: _formKey,
+                child: SizedBox(
+                  height: bodyHeight * 0.4,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      TextFieldInputWidget(
+                        nameController: amount,
+                        label: "Jumlah Penarikan",
+                        obscureCondition: false,
+                        keyboardNext: true,
+                        keyboard: false,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      ButtonWidget(
+                        buttonText: "Tarik Saldo",
+                        colorSetBody: Colors.blue,
+                        colorSetText: Colors.white,
+                        functionTap: () async {
+                          FocusScope.of(context).unfocus();
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => const SuccessScreen()));
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ]),
+          ),
+        ),
+      ),
+    );
+  }
+}
