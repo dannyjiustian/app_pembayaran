0733637bcfef873d0989a0c560f45cb76eb3ab39
dannyjiustian <danny.djie72@gmail.com>
2024-03-16 00:12:09 +0700

    add new function to convert interger to rupiah format string

---
+++ lib/Function/formatToRupiah.dart
@@ -0,0 +1,19 @@
+String formatToRupiah(int balance, {int? type}) {
+  String formattedBalance = balance.toString();
+  List<String> parts = [];
+
+  while (formattedBalance.length > 3) {
+    parts.add(formattedBalance.substring(formattedBalance.length - 3));
+    formattedBalance =
+        formattedBalance.substring(0, formattedBalance.length - 3);
+  }
+  parts.add(formattedBalance);
+
+  String text = (type == 0)
+      ? "-Rp. "
+      : (type == 1)
+          ? "+Rp. "
+          : "Rp. ";
+
+  return "${text}${parts.reversed.join('.')}";
+}
