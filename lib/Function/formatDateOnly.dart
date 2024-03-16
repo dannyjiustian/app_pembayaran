027f33c30501355e4eee9ece392e879645e39fc3
dannyjiustian <danny.djie72@gmail.com>
2024-03-16 21:50:46 +0700

    add new function for convert the date only

---
+++ lib/Function/formatDateOnly.dart
@@ -0,0 +1,25 @@
+String formatDateOnly(String? dateString) {
+  if (dateString == null) {
+    dateString = "2024-01-01T12:49:37.517Z";
+  }
+  DateTime date = DateTime.parse(dateString);
+
+  final List<String> indonesianMonths = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'Mei',
+    'Jun',
+    'Jul',
+    'Agu',
+    'Sep',
+    'Okt',
+    'Nov',
+    'Des'
+  ];
+
+  String formattedDate =
+      '${date.day.toString().padLeft(2, '0')} ${indonesianMonths[date.month - 1]} ${date.year}';
+  return formattedDate;
+}
