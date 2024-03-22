5ae6d016c974c3a5e5a7c22f4362c2cd4b475ba4
dannyjiustian <danny.djie72@gmail.com>
2024-03-22 20:12:18 +0700

    [PROFILE SCREEN] remove the material app widget

--- lib/Views/Profile/ProfileScreen.dart
+++ lib/Views/Profile/ProfileScreen.dart
@@ -99,169 +99,159 @@ class _ProfileScreenState extends State<ProfileScreen> {
     final bodyHeight = mediaQueryHeight -
         appBar.preferredSize.height -
         MediaQuery.of(context).padding.top;
-    return MaterialApp(
-        title: "Profile",
-        theme: ThemeData(
-          textTheme: GoogleFonts.poppinsTextTheme(
-            Theme.of(context).textTheme,
-          ),
-          fontFamily: GoogleFonts.poppins().fontFamily,
-        ),
-        home: Scaffold(
-          appBar: appBar,
-          body: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
+    return Scaffold(
+      appBar: appBar,
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    IconAppbarCustomWidget(
-                      iconType: Iconsax.arrow_left_2,
-                      functionTap: () async {
-                        widget.refreshToken();
-                        Navigator.of(context).pop();
-                      },
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 20),
-                      child: Text(
-                        "Kembali",
-                        style: GoogleFonts.poppins(
-                            fontSize: 14, fontWeight: FontWeight.w600),
-                      ),
-                    ),
-                  ],
+                IconAppbarCustomWidget(
+                  iconType: Iconsax.arrow_left_2,
+                  functionTap: () async {
+                    widget.refreshToken();
+                    Navigator.of(context).pop();
+                  },
                 ),
-                Expanded(
-                  child: Form(
-                    key: _formKey,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        TextFieldInputWidget(
-                          nameController: nameController,
-                          label: "Nama Lengkap",
-                          obscureCondition: false,
-                          keyboardNext: true,
-                          keyboard: false,
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        TextFieldInputWidget(
-                          nameController: usernameController,
-                          label: "Username",
-                          obscureCondition: false,
-                          keyboardNext: true,
-                          keyboard: false,
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        TextFieldInputWidget(
-                          nameController: emailController,
-                          label: "Email",
-                          obscureCondition: false,
-                          keyboardNext: false,
-                          keyboard: true,
-                        ),
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              _loading
-                                  ? const LoadingWidget()
-                                  : ButtonWidget(
-                                      buttonText: "Simpan Perubahan",
-                                      colorSetBody: Colors.blue,
-                                      colorSetText: Colors.white,
-                                      functionTap: () async {
-                                        FocusScope.of(context).unfocus();
-                                        if (_formKey.currentState!.validate()) {
-                                          SharedPreferences localStorage =
-                                              await SharedPreferences
-                                                  .getInstance();
-                                          setState(() {
-                                            _loading = true;
-                                          });
-                                          if (jwtData!.name ==
-                                                  nameController.text &&
-                                              jwtData!.username ==
-                                                  usernameController.text &&
-                                              jwtData!.email ==
-                                                  emailController.text) {
-                                            QuickAlert.show(
-                                              context: context,
-                                              type: QuickAlertType.error,
-                                              title: 'Oops...',
-                                              text: 'Tidak ada perubahan data!',
-                                            );
-                                          } else {
-                                            UpdateUser data = UpdateUser(
-                                                id_user: jwtData!.id_user,
-                                                name: nameController.text,
-                                                username:
-                                                    usernameController.text,
-                                                email: emailController.text);
-                                            var res = await conn.updateAction(
-                                                accessToken!, data.toJson());
-                                            if (res.status) {
-                                              localStorage.setString(
-                                                  'accessToken',
-                                                  (res.data?.accessToken)
-                                                      .toString());
-                                              localStorage.setString(
-                                                  'refreshToken',
-                                                  (res.data?.refreshToken)
-                                                      .toString());
-                                              QuickAlert.show(
-                                                context: context,
-                                                type: QuickAlertType.success,
-                                                title: "Berhasil",
-                                                text: "Update Data Berhasil",
-                                                barrierDismissible: false,
-                                              ).then((value) {
-                                                refreshData();
-                                              });
-                                            } else {
-                                              AlertWidget(
-                                                      mediaQueryWidth,
-                                                      Iconsax.close_square,
-                                                      Colors.red.shade300,
-                                                      "Error!",
-                                                      "${res!.message}")
-                                                  .show(context);
-                                            }
-                                          }
-                                          setState(() {
-                                            _loading = false;
-                                          });
-                                        } else {
-                                          QuickAlert.show(
-                                            context: context,
-                                            type: QuickAlertType.error,
-                                            title: 'Oops...',
-                                            text:
-                                                'Harap isi input field yang diperlukan / format salah!',
-                                          );
-                                        }
-                                      },
-                                    ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Text(
+                    "Kembali",
+                    style: GoogleFonts.poppins(
+                        fontSize: 14, fontWeight: FontWeight.w600),
                   ),
                 ),
               ],
             ),
-          ),
-        ));
+            Expanded(
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    TextFieldInputWidget(
+                      nameController: nameController,
+                      label: "Nama Lengkap",
+                      obscureCondition: false,
+                      keyboardNext: true,
+                      keyboard: false,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextFieldInputWidget(
+                      nameController: usernameController,
+                      label: "Username",
+                      obscureCondition: false,
+                      keyboardNext: true,
+                      keyboard: false,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextFieldInputWidget(
+                      nameController: emailController,
+                      label: "Email",
+                      obscureCondition: false,
+                      keyboardNext: false,
+                      keyboard: true,
+                    ),
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          _loading
+                              ? const LoadingWidget()
+                              : ButtonWidget(
+                                  buttonText: "Simpan Perubahan",
+                                  colorSetBody: Colors.blue,
+                                  colorSetText: Colors.white,
+                                  functionTap: () async {
+                                    FocusScope.of(context).unfocus();
+                                    if (_formKey.currentState!.validate()) {
+                                      SharedPreferences localStorage =
+                                          await SharedPreferences.getInstance();
+                                      setState(() {
+                                        _loading = true;
+                                      });
+                                      if (jwtData!.name ==
+                                              nameController.text &&
+                                          jwtData!.username ==
+                                              usernameController.text &&
+                                          jwtData!.email ==
+                                              emailController.text) {
+                                        QuickAlert.show(
+                                          context: context,
+                                          type: QuickAlertType.error,
+                                          title: 'Oops...',
+                                          text: 'Tidak ada perubahan data!',
+                                        );
+                                      } else {
+                                        UpdateUser data = UpdateUser(
+                                            id_user: jwtData!.id_user,
+                                            name: nameController.text,
+                                            username: usernameController.text,
+                                            email: emailController.text);
+                                        var res = await conn.updateAction(
+                                            accessToken!, data.toJson());
+                                        if (res.status) {
+                                          localStorage.setString(
+                                              'accessToken',
+                                              (res.data?.accessToken)
+                                                  .toString());
+                                          localStorage.setString(
+                                              'refreshToken',
+                                              (res.data?.refreshToken)
+                                                  .toString());
+                                          QuickAlert.show(
+                                            context: context,
+                                            type: QuickAlertType.success,
+                                            title: "Berhasil",
+                                            text: "Update Data Berhasil",
+                                            barrierDismissible: false,
+                                          ).then((value) {
+                                            refreshData();
+                                          });
+                                        } else {
+                                          AlertWidget(
+                                                  mediaQueryWidth,
+                                                  Iconsax.close_square,
+                                                  Colors.red.shade300,
+                                                  "Error!",
+                                                  "${res!.message}")
+                                              .show(context);
+                                        }
+                                      }
+                                      setState(() {
+                                        _loading = false;
+                                      });
+                                    } else {
+                                      QuickAlert.show(
+                                        context: context,
+                                        type: QuickAlertType.error,
+                                        title: 'Oops...',
+                                        text:
+                                            'Harap isi input field yang diperlukan / format salah!',
+                                      );
+                                    }
+                                  },
+                                ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
