e3ff0e852b8ad5b71618dc491c642770ea65cf07
dannyjiustian <danny.djie72@gmail.com>
2024-03-22 20:04:41 +0700

    [DETECT CARD SCREEN] update the class name and add function for detect NFC

--- lib/Views/Card/DetectCardScreen.dart
+++ lib/Views/Card/DetectCardScreen.dart
@@ -1,22 +1,74 @@
-import 'package:app_pembayaran/Views/TopUp/FormTopUpScreen.dart';
+import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:lottie/lottie.dart';
+import 'package:nfc_manager/nfc_manager.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-import '../DaftarKartu/DaftarKartuScreen.dart';
+import '../../Connection/Connection.dart';
+import '../../Models/JWT/JWTDecode.dart';
+import '../Widget/LoadingNFCReaderWidget.dart';
+import 'FormDetectScreen.dart';
+import '../Widget/ButtonWidget.dart';
 import '../Widget/IconAppbarCostuimeWidget.dart';
 
 class DetectCardScreen extends StatefulWidget {
-  const DetectCardScreen({super.key});
+  const DetectCardScreen({
+    super.key,
+    required this.navigatorKey,
+    this.detectCard = true,
+    required this.typeDetect,
+    this.refreshToken,
+  });
+
+  final GlobalKey<NavigatorState> navigatorKey;
+  final bool detectCard;
+  final int typeDetect;
+  final VoidCallback? refreshToken;
 
   @override
-  State<DetectCardScreen> createState() => _DetectCardScreenState();
+  State<DetectCardScreen> createState() => _DetectCardScreenState(detectCard);
 }
 
+JWTDecode? jwtData;
+String? accessToken;
+
 class _DetectCardScreenState extends State<DetectCardScreen> {
+  Connection conn = Connection();
+  // bool detectCard = true;
+  bool _isMounted = false;
+  late bool _detectCard;
+
+  _DetectCardScreenState(bool detectCard) {
+    _detectCard = detectCard;
+  }
+
+  Future checkLocalStorage() async {
+    final pref = await SharedPreferences.getInstance();
+    accessToken = pref.getString('accessToken');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _isMounted = true;
+    _detectCard = widget.detectCard;
+    checkLocalStorage().then((value) {
+      setState(() {
+        jwtData =
+            JWTDecode.fromJson(JWT.decode(accessToken.toString()).payload);
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _isMounted = false;
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final mediaQueryHeight = MediaQuery.of(context).size.height;
@@ -41,14 +93,20 @@ class _DetectCardScreenState extends State<DetectCardScreen> {
     final bodyHeight = mediaQueryHeight -
         appBar.preferredSize.height -
         MediaQuery.of(context).padding.top;
-    return MaterialApp(
-        title: "Deteksi Kartu",
-        theme: ThemeData(
-          textTheme: GoogleFonts.poppinsTextTheme(
-            Theme.of(context).textTheme,
-          ),
-        ),
-        home: Scaffold(
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (bool didPop) async {
+        if (didPop) {
+          return;
+        }
+        bool isNfcAvailable = await NfcManager.instance.isAvailable();
+        if (isNfcAvailable) {
+          await NfcManager.instance.stopSession();
+        }
+        widget.refreshToken!();
+        Navigator.of(context).pop();
+      },
+      child: Scaffold(
           appBar: appBar,
           body: Padding(
             padding: const EdgeInsets.all(20),
@@ -60,6 +118,12 @@ class _DetectCardScreenState extends State<DetectCardScreen> {
                   IconAppbarCustomWidget(
                     iconType: Iconsax.arrow_left_2,
                     functionTap: () async {
+                      bool isNfcAvailable =
+                          await NfcManager.instance.isAvailable();
+                      if (isNfcAvailable) {
+                        await NfcManager.instance.stopSession();
+                      }
+                      widget.refreshToken!();
                       Navigator.of(context).pop();
                     },
                   ),
@@ -74,41 +138,192 @@ class _DetectCardScreenState extends State<DetectCardScreen> {
                 ],
               ),
               Expanded(
-                  child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Lottie.asset('assets/img/lottie/contactless.json'),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  InkWell(
-                    onTap: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => FormTopUpScreen()));
-                    },
-                    child: Text(
-                      "Tempelkan kartu Kamu ke area NFC Ponsel",
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.poppins(
-                          fontSize: 18, fontWeight: FontWeight.w600),
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => DaftarKartuScreen()));
-                    },
-                    child: Text(
-                      "Tempelkan kartu Kamu ke area NFC Ponsel",
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.poppins(
-                          fontSize: 18, fontWeight: FontWeight.w600),
-                    ),
-                  ),
-                ],
-              ))
+                  child: FutureBuilder<bool>(
+                      future: NfcManager.instance.isAvailable(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return LoadingNFCReaderWidget(
+                              mediaQueryWidth: mediaQueryWidth);
+                        } else if (snapshot.data != true) {
+                          return Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Lottie.asset(
+                                  'assets/img/lottie/device_error.json'),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              Text(
+                                "NFC tidak tersedia atau NFC dalam posisi mati, silakan nyalakan dan coba lagi!",
+                                textAlign: TextAlign.center,
+                                style: GoogleFonts.poppins(
+                                    fontSize: 18, fontWeight: FontWeight.w600),
+                              ),
+                            ],
+                          );
+                        } else {
+                          NfcManager.instance.startSession(
+                              onDiscovered: (NfcTag tag) async {
+                            Set<String> identifiers = {};
+
+                            // Iterate over each data item in the tag
+                            tag.data.forEach((key, value) {
+                              if (value.containsKey('identifier') &&
+                                  value['identifier'] is List<int>) {
+                                // Convert byte array to hexadecimal string
+                                String identifier = value['identifier']
+                                    .map((byte) =>
+                                        byte.toRadixString(16).padLeft(2, '0'))
+                                    .join(':')
+                                    .toUpperCase(); // Convert to uppercase
+                                // Add the identifier to the set
+                                identifiers.add(identifier);
+                              }
+                            });
+
+                            // Convert the set of identifiers to a single string
+                            String identifiersString = identifiers.join('');
+
+                            // Check if there are any identifiers
+                            if (identifiers.isNotEmpty) {
+                              // Print the unique identifiers without brackets
+                              // print(identifiersString);
+                              var res;
+                              if (widget.typeDetect == 1) {
+                                res = await conn.getCardByIDUser(
+                                    accessToken!, jwtData!.id_user,
+                                    id_rfid: identifiersString);
+                              } else {
+                                res = await conn.searchCard(
+                                    accessToken!, identifiersString);
+                              }
+                              if (_isMounted) {
+                                if (res.status) {
+                                  setState(() {
+                                    checkLocalStorage();
+                                  });
+                                  widget.navigatorKey.currentState
+                                      ?.pushReplacement(
+                                    MaterialPageRoute(
+                                        builder: (context) => FormDetectScreen(
+                                              refreshToken: widget.refreshToken,
+                                              idRfid: widget.typeDetect == 1
+                                                  ? ""
+                                                  : identifiersString,
+                                              walletAddress:
+                                                  widget.typeDetect == 1
+                                                      ? res.data[0]
+                                                          .wallet_address
+                                                      : "",
+                                              id_card: widget.typeDetect == 1
+                                                  ? res.data[0].id_card
+                                                  : "",
+                                              balance: widget.typeDetect == 1
+                                                  ? res.data[0].balance
+                                                  : 0,
+                                              typeDetect: widget.typeDetect,
+                                            )),
+                                  );
+                                } else {
+                                  setState(() {
+                                    _detectCard = false;
+                                    checkLocalStorage();
+                                  });
+                                }
+                              } else {
+                                if (res.status) {
+                                  checkLocalStorage();
+                                  widget.navigatorKey.currentState
+                                      ?.pushReplacement(
+                                    MaterialPageRoute(
+                                        builder: (context) => FormDetectScreen(
+                                              refreshToken: widget.refreshToken,
+                                              idRfid: widget.typeDetect == 1
+                                                  ? ""
+                                                  : identifiersString,
+                                              walletAddress:
+                                                  widget.typeDetect == 1
+                                                      ? res.data[0]
+                                                          .wallet_address
+                                                      : "",
+                                              id_card: widget.typeDetect == 1
+                                                  ? res.data[0].id_card
+                                                  : "",
+                                              balance: widget.typeDetect == 1
+                                                  ? res.data[0].balance
+                                                  : 0,
+                                              typeDetect: widget.typeDetect,
+                                            )),
+                                  );
+                                } else {
+                                  widget.navigatorKey.currentState
+                                      ?.pushReplacement(
+                                    MaterialPageRoute(
+                                      builder: (context) => DetectCardScreen(
+                                        navigatorKey: widget.navigatorKey,
+                                        detectCard: false,
+                                        typeDetect: widget.typeDetect,
+                                        refreshToken: widget.refreshToken,
+                                      ),
+                                    ),
+                                  );
+                                }
+                              }
+                            }
+                          });
+                          return _detectCard
+                              ? Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Lottie.asset(
+                                        'assets/img/lottie/contactless.json'),
+                                    const SizedBox(
+                                      height: 20,
+                                    ),
+                                    Text(
+                                      "Tempelkan kartu Kamu ke area NFC Ponsel",
+                                      textAlign: TextAlign.center,
+                                      style: GoogleFonts.poppins(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                  ],
+                                )
+                              : Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Lottie.asset(
+                                        'assets/img/lottie/nfc_read_false.json'),
+                                    const SizedBox(
+                                      height: 20,
+                                    ),
+                                    Text(
+                                      "${widget.typeDetect == 1 ? "Kartu Belum Terdaftar Diakun kamu" : "Kartu Sudah Terdaftar Diakun lain"}, Silakan coba dengan kartu lain!",
+                                      textAlign: TextAlign.center,
+                                      style: GoogleFonts.poppins(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                    const SizedBox(
+                                      height: 20,
+                                    ),
+                                    ButtonWidget(
+                                        buttonText: "Coba Lagi",
+                                        colorSetBody: Colors.blue.shade100,
+                                        colorSetText: Colors.black,
+                                        functionTap: () {
+                                          setState(() {
+                                            _detectCard = true;
+                                          });
+                                        }),
+                                  ],
+                                );
+                        }
+                        // }
+                      })),
             ]),
-          ),
-        ));
+          )),
+    );
   }
 }
