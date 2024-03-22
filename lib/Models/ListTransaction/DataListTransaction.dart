fdb33e0eb3342f2cd94e5c9133705d1e70a183a1
dannyjiustian <danny.djie72@gmail.com>
2024-03-22 20:01:42 +0700

    [MODEL DATA LIST TRANSACTION] modify the data required

--- lib/Models/ListTransaction/DataListTransaction.dart
+++ lib/Models/ListTransaction/DataListTransaction.dart
@@ -4,16 +4,16 @@ part 'DataListTransaction.g.dart';
 
 @JsonSerializable()
 class DataListTransaction {
-  final String id_transaction, id_card, id_user, id_hardware, id_outlet, updated_at, status;
-  final String? txn_hash;
+  final String id_transaction, id_card, id_user, updated_at, status;
+  final String? txn_hash, id_hardware, id_outlet;
   final int total_payment, type;
 
   DataListTransaction({
     required this.id_transaction,
     required this.id_card,
     required this.id_user,
-    required this.id_hardware,
-    required this.id_outlet,
+    this.id_hardware,
+    this.id_outlet,
     this.txn_hash,
     required this.total_payment,
     required this.type,
