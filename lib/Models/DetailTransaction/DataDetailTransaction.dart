31239d4c1a20fea8c9d9a51a35fde64bc9c9a660
dannyjiustian <danny.djie72@gmail.com>
2024-03-23 22:57:25 +0700

    [DATA DETAIL TRANSACTION MODELS] remove required id card

--- lib/Models/DetailTransaction/DataDetailTransaction.dart
+++ lib/Models/DetailTransaction/DataDetailTransaction.dart
@@ -4,13 +4,13 @@ part 'DataDetailTransaction.g.dart';
 
 @JsonSerializable()
 class DataDetailTransaction {
-  final String id_transaction, id_card, id_user, updated_at, status;
-  final String? txn_hash, id_hardware, id_outlet;
+  final String id_transaction, id_user, updated_at, status;
+  final String? id_card, txn_hash, id_hardware, id_outlet;
   final int total_payment, type;
 
   DataDetailTransaction({
     required this.id_transaction,
-    required this.id_card,
+    this.id_card,
     required this.id_user,
     this.id_hardware,
     this.id_outlet,
