59c8b2cdd8394ad1be3c502a6a9819e328a4cd1a
dannyjiustian <danny.djie72@gmail.com>
2024-03-24 23:56:11 +0700

    [DATA LIST READER MODELS] add new data for mapping json

--- lib/Models/ListReader/DataListReader.dart
+++ lib/Models/ListReader/DataListReader.dart
@@ -1,11 +1,14 @@
 import 'package:json_annotation/json_annotation.dart';
 
+import '../DetailTransaction/DataDetailTransaction.dart';
+
 part 'DataListReader.g.dart';
 
 @JsonSerializable()
 class DataListReader {
-  final String id_hardware, id_user, updated_at, sn_sensor, name;
+  final String id_hardware, id_user, created_at, updated_at, sn_sensor, name;
   final bool is_active;
+  final List<DataDetailTransaction> transactions;
 
   DataListReader({
     required this.id_hardware,
@@ -13,7 +16,9 @@ class DataListReader {
     required this.sn_sensor,
     required this.name,
     required this.is_active,
+    required this.created_at,
     required this.updated_at,
+    required this.transactions,
   });
 
   factory DataListReader.fromJson(Map<String, dynamic> json) =>
--- lib/Models/ListReader/DataListReader.g.dart
+++ lib/Models/ListReader/DataListReader.g.dart
@@ -13,15 +13,21 @@ DataListReader _$DataListReaderFromJson(Map<String, dynamic> json) =>
       sn_sensor: json['sn_sensor'] as String,
       name: json['name'] as String,
       is_active: json['is_active'] as bool,
+      created_at: json['created_at'] as String,
       updated_at: json['updated_at'] as String,
+      transactions: (json['transactions'] as List<dynamic>)
+          .map((e) => DataDetailTransaction.fromJson(e as Map<String, dynamic>))
+          .toList(),
     );
 
 Map<String, dynamic> _$DataListReaderToJson(DataListReader instance) =>
     <String, dynamic>{
       'id_hardware': instance.id_hardware,
       'id_user': instance.id_user,
+      'created_at': instance.created_at,
       'updated_at': instance.updated_at,
       'sn_sensor': instance.sn_sensor,
       'name': instance.name,
       'is_active': instance.is_active,
+      'transactions': instance.transactions,
     };
