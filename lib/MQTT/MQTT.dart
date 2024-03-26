57c8da74f1a35dd4cc1ce41189bd14afe2b0bda2
dannyjiustian <danny.djie72@gmail.com>
2024-03-26 23:16:16 +0700

    [MQTT] add new mqtt connection for the dart

---
+++ lib/MQTT/MQTT.dart
@@ -0,0 +1,97 @@
+import 'dart:io';
+
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
+
+enum MqttCurrentConnectionState {
+  IDLE,
+  CONNECTING,
+  CONNECTED,
+  DISCONNECTED,
+  ERROR_WHEN_CONNECTING
+}
+
+enum MqttSubscriptionState { IDLE, SUBSCRIBED }
+
+typedef MessageCallback = void Function(String message);
+
+class MQTTClientWrapper {
+  late MqttServerClient client;
+  MqttCurrentConnectionState connectionState = MqttCurrentConnectionState.IDLE;
+  MqttSubscriptionState subscriptionState = MqttSubscriptionState.IDLE;
+  late MessageCallback messageCallback;
+
+  Future<void> prepareMqttClient(String topic, MessageCallback callback) async {
+    messageCallback = callback;
+    _setupMqttClient();
+    await _connectClient();
+    if (connectionState == MqttCurrentConnectionState.CONNECTED) {
+      _subscribeToTopic(topic);
+    } else {
+      print('Client is not connected. Cannot subscribe to topic.');
+    }
+  }
+
+  Future<void> _connectClient() async {
+    try {
+      print('Client connecting....');
+      connectionState = MqttCurrentConnectionState.CONNECTING;
+      await client.connect('testingbroker', 'testingbroker');
+    } on Exception catch (e) {
+      print('Client exception - $e');
+      connectionState = MqttCurrentConnectionState.ERROR_WHEN_CONNECTING;
+      client.disconnect();
+    }
+
+    if (client.connectionStatus?.state == MqttConnectionState.connected) {
+      connectionState = MqttCurrentConnectionState.CONNECTED;
+      print('Client connected');
+    } else {
+      print(
+          'ERROR client connection failed - disconnecting, status is ${client.connectionStatus}');
+      connectionState = MqttCurrentConnectionState.ERROR_WHEN_CONNECTING;
+      client.disconnect();
+    }
+  }
+
+  void _setupMqttClient() {
+    client = MqttServerClient.withPort(
+        '21a415c74e5f429db8def0c60728bb0d.s1.eu.hivemq.cloud',
+        'testingbroker',
+        8883);
+    client.secure = true;
+    client.securityContext = SecurityContext.defaultContext;
+    client.keepAlivePeriod = 20;
+    client.onDisconnected = _onDisconnected;
+    client.onConnected = _onConnected;
+    client.onSubscribed = _onSubscribed;
+  }
+
+  void _subscribeToTopic(String topicName) {
+    print('Subscribing to the $topicName topic');
+    client.subscribe(topicName, MqttQos.atMostOnce);
+
+    client.updates?.listen((List<MqttReceivedMessage<MqttMessage>> c) {
+      final MqttPublishMessage recMess = c[0].payload as MqttPublishMessage;
+      var message =
+          MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+      // print(message);
+      messageCallback(message);
+    });
+  }
+
+  void _onSubscribed(String topic) {
+    print('Subscription confirmed for topic $topic');
+    subscriptionState = MqttSubscriptionState.SUBSCRIBED;
+  }
+
+  void _onDisconnected() {
+    print('OnDisconnected client callback - Client disconnection');
+    connectionState = MqttCurrentConnectionState.DISCONNECTED;
+  }
+
+  void _onConnected() {
+    connectionState = MqttCurrentConnectionState.CONNECTED;
+    print('OnConnected client callback - Client connection was successful');
+  }
+}
