a3401c076452549aea013a7e3a91da0c36869ce9
dannyjiustian <danny.djie72@gmail.com>
2024-03-28 23:49:12 +0700

    [DATA OUTLET DATA MODEL] add new params for balance eth and wallet address

--- lib/Models/OutletData/DataOutletData.dart
+++ lib/Models/OutletData/DataOutletData.dart
@@ -4,14 +4,17 @@ part 'DataOutletData.g.dart';
 
 @JsonSerializable()
 class DataOutletData {
-  final String id_outlet, updated_at, smart_contract;
+  final String id_outlet, updated_at, smart_contract, wallet_address;
   final int balance;
+  final double balance_eth;
 
   DataOutletData({
     required this.id_outlet,
     required this.updated_at,
     required this.smart_contract,
     required this.balance,
+    required this.wallet_address,
+    required this.balance_eth,
   });
 
   factory DataOutletData.fromJson(Map<String, dynamic> json) =>
--- lib/Models/OutletData/DataOutletData.g.dart
+++ lib/Models/OutletData/DataOutletData.g.dart
@@ -12,6 +12,8 @@ DataOutletData _$DataOutletDataFromJson(Map<String, dynamic> json) =>
       updated_at: json['updated_at'] as String,
       smart_contract: json['smart_contract'] as String,
       balance: json['balance'] as int,
+      wallet_address: json['wallet_address'] as String,
+      balance_eth: (json['balance_eth'] as num).toDouble(),
     );
 
 Map<String, dynamic> _$DataOutletDataToJson(DataOutletData instance) =>
@@ -19,5 +21,7 @@ Map<String, dynamic> _$DataOutletDataToJson(DataOutletData instance) =>
       'id_outlet': instance.id_outlet,
       'updated_at': instance.updated_at,
       'smart_contract': instance.smart_contract,
+      'wallet_address': instance.wallet_address,
       'balance': instance.balance,
+      'balance_eth': instance.balance_eth,
     };
