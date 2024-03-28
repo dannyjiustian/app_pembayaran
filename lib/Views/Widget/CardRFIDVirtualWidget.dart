d0c69e2b2e587da2e78689d430b47c47fc73689b
dannyjiustian <danny.djie72@gmail.com>
2024-03-28 23:50:51 +0700

    [CARD RFID VIRTUAL WIDGET] add new view for balance eth and faucet

--- lib/Views/Widget/CardRFIDVirtualWidget.dart
+++ lib/Views/Widget/CardRFIDVirtualWidget.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:quickalert/quickalert.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../Function/cutString.dart';
 import '../../Function/formatToRupiah.dart';
+import 'ButtonWidget.dart';
 
 class CardRFIDVirtualWidget extends StatelessWidget {
   const CardRFIDVirtualWidget({
@@ -11,11 +16,23 @@ class CardRFIDVirtualWidget extends StatelessWidget {
     required this.mediaQueryWidth,
     required this.saldo,
     this.walletAddress,
+    this.balanceEth,
   });
 
   final String? walletAddress;
   final double mediaQueryWidth;
   final int saldo;
+  final double? balanceEth;
+
+  Future<void> _launchInBrowser(String urlString) async {
+    Uri url = Uri.parse(urlString);
+    if (!await launchUrl(
+      url,
+      mode: LaunchMode.externalApplication,
+    )) {
+      throw Exception('Could not launch $url');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,12 +49,154 @@ class CardRFIDVirtualWidget extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              "Kartu Pembayaran",
-              style: GoogleFonts.poppins(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.black),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Kartu Pembayaran",
+                  style: GoogleFonts.poppins(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black),
+                ),
+                balanceEth != null
+                    ? Row(
+                        children: [
+                          Text(
+                            "Ketuk Untuk Info",
+                            style: GoogleFonts.poppins(
+                                fontSize: 10, color: Colors.black),
+                          ),
+                          const SizedBox(width: 5),
+                          InkWell(
+                            onTap: () {
+                              QuickAlert.show(
+                                context: context,
+                                type: QuickAlertType.custom,
+                                barrierDismissible: true,
+                                confirmBtnText: 'Tutup',
+                                showConfirmBtn: false,
+                                customAsset: 'assets/img/gif/info.gif',
+                                widget: Column(
+                                  children: [
+                                    Text(
+                                      "Detail Etherium Wallet",
+                                      style: GoogleFonts.poppins(
+                                          fontSize: 14, color: Colors.black),
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text("Wallet Address",
+                                            style: GoogleFonts.poppins(
+                                              fontSize: 14,
+                                            )),
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.end,
+                                          children: [
+                                            Text(
+                                                cutString(walletAddress!,
+                                                    change: "..."),
+                                                style: GoogleFonts.poppins(
+                                                    fontSize: 14,
+                                                    fontWeight:
+                                                        FontWeight.w600)),
+                                            const SizedBox(
+                                              width: 2,
+                                            ),
+                                            InkWell(
+                                              onTap: () {
+                                                Clipboard.setData(ClipboardData(
+                                                        text: walletAddress!))
+                                                    .then((_) {
+                                                  Fluttertoast.showToast(
+                                                    msg:
+                                                        'Wallet Address Tersalin',
+                                                    toastLength:
+                                                        Toast.LENGTH_SHORT,
+                                                    gravity:
+                                                        ToastGravity.BOTTOM,
+                                                    timeInSecForIosWeb: 1,
+                                                    backgroundColor:
+                                                        Colors.black,
+                                                    textColor: Colors.white,
+                                                    fontSize: 16.0,
+                                                  );
+                                                });
+                                              },
+                                              child: const Icon(
+                                                  Iconsax.document_copy,
+                                                  size: 20),
+                                            )
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text("Saldo ETH",
+                                            style: GoogleFonts.poppins(
+                                              fontSize: 14,
+                                            )),
+                                        Text("${balanceEth!} ETH",
+                                            style: GoogleFonts.poppins(
+                                                fontSize: 14,
+                                                fontWeight: FontWeight.w600)),
+                                      ],
+                                    ),
+                                    const SizedBox(
+                                      height: 20,
+                                    ),
+                                    Text(
+                                      "Tambah Saldo Etherium",
+                                      style: GoogleFonts.poppins(
+                                          fontSize: 14, color: Colors.black),
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    ButtonWidget(
+                                        buttonText: "Alchamy Faucet",
+                                        colorSetBody: Colors.blue,
+                                        colorSetText: Colors.white,
+                                        functionTap: () {
+                                          _launchInBrowser(
+                                              "https://www.alchemy.com/faucets/ethereum-sepolia");
+                                        }),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    ButtonWidget(
+                                        buttonText: "PoW Faucet",
+                                        colorSetBody: Colors.blue,
+                                        colorSetText: Colors.white,
+                                        functionTap: () {
+                                          _launchInBrowser(
+                                              "https://sepolia-faucet.pk910.de/");
+                                        })
+                                  ],
+                                ),
+                                onConfirmBtnTap: () async {
+                                  Navigator.of(context, rootNavigator: true)
+                                      .pop();
+                                },
+                              );
+                            },
+                            child: const Icon(
+                              Iconsax.info_circle,
+                            ),
+                          ),
+                        ],
+                      )
+                    : const SizedBox()
+              ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
@@ -53,6 +212,7 @@ class CardRFIDVirtualWidget extends StatelessWidget {
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
                 Column(
                     mainAxisAlignment: MainAxisAlignment.start,
@@ -70,6 +230,26 @@ class CardRFIDVirtualWidget extends StatelessWidget {
                             fontWeight: FontWeight.w600,
                             color: Colors.black),
                       ),
+                      balanceEth != null
+                          ? Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Sisa Saldo ETH",
+                                  style: GoogleFonts.poppins(
+                                      fontSize: 10, color: Colors.black),
+                                ),
+                                Text(
+                                  '${balanceEth!} ETH',
+                                  style: GoogleFonts.poppins(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w600,
+                                      color: Colors.black),
+                                ),
+                              ],
+                            )
+                          : const SizedBox()
                     ]),
                 walletAddress != null
                     ? Text(
@@ -79,7 +259,7 @@ class CardRFIDVirtualWidget extends StatelessWidget {
                             fontWeight: FontWeight.w600,
                             color: Colors.black),
                       )
-                    : SizedBox(),
+                    : const SizedBox(),
               ],
             )
           ],
