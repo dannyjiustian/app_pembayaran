79aa0c4a1362445c9bfef3cd7c3ff9494723efbb
dannyjiustian <danny.djie72@gmail.com>
2024-04-04 21:37:51 +0700

    [CARD BANNER WIDGET] change the card file for the banner information

--- lib/Views/Widget/CardBannerWidget.dart
+++ lib/Views/Widget/CardBannerWidget.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
 
-class CardBannerWidget extends StatelessWidget {
-  const CardBannerWidget({
+class CardBannerWidget extends StatefulWidget {
+  CardBannerWidget({
     super.key,
+    required this.imageFile,
   });
 
+  final String imageFile;
+
+  @override
+  State<CardBannerWidget> createState() => _CardBannerWidgetState();
+}
+
+class _CardBannerWidgetState extends State<CardBannerWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 2.5),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
-        border: Border.all(color: Colors.grey.shade300),
         color: Colors.blue.shade100,
-        image: const DecorationImage(
-          image: NetworkImage(
-              "https://images.tokopedia.net/img/cache/1208/NsjrJu/2024/3/5/71ceb9ec-a589-4faa-b0b4-769956548964.jpg"),
+        image: DecorationImage(
+          image: AssetImage(widget.imageFile),
           fit: BoxFit.cover,
         ),
       ),
