1cb4fa2a3ac79ff216dfb72587f1ec27e235369b
dannyjiustian <danny.djie72@gmail.com>
2024-04-04 21:31:45 +0700

    [DATA LIST CARD MODEL] modify the file add new value from api

--- lib/Models/ListCard/DataListCard.dart
+++ lib/Models/ListCard/DataListCard.dart
@@ -1,12 +1,16 @@
 import 'package:json_annotation/json_annotation.dart';
 
+import '../DetailTransaction/DataDetailTransaction.dart';
+
 part 'DataListCard.g.dart';
 
 @JsonSerializable()
 class DataListCard {
-  final String id_card, id_user, id_rfid, wallet_address;
+  final String id_card, id_user, id_rfid, wallet_address, created_at;
   final int balance;
   final double balance_eth;
+  final bool is_active;
+  final List<DataDetailTransaction>? transactions;
 
   DataListCard({
     required this.id_card,
@@ -15,6 +19,9 @@ class DataListCard {
     required this.wallet_address,
     required this.balance_eth,
     required this.balance,
+    required this.is_active,
+    required this.created_at,
+    this.transactions,
   });
 
   factory DataListCard.fromJson(Map<String, dynamic> json) =>
--- lib/Models/ListCard/DataListCard.g.dart
+++ lib/Models/ListCard/DataListCard.g.dart
@@ -13,6 +13,12 @@ DataListCard _$DataListCardFromJson(Map<String, dynamic> json) => DataListCard(
       wallet_address: json['wallet_address'] as String,
       balance_eth: (json['balance_eth'] as num).toDouble(),
       balance: json['balance'] as int,
+      is_active: json['is_active'] as bool,
+      created_at: json['created_at'] as String,
+      transactions: (json['transactions'] as List<dynamic>?)
+          ?.map(
+              (e) => DataDetailTransaction.fromJson(e as Map<String, dynamic>))
+          .toList(),
     );
 
 Map<String, dynamic> _$DataListCardToJson(DataListCard instance) =>
@@ -21,6 +27,9 @@ Map<String, dynamic> _$DataListCardToJson(DataListCard instance) =>
       'id_user': instance.id_user,
       'id_rfid': instance.id_rfid,
       'wallet_address': instance.wallet_address,
+      'created_at': instance.created_at,
       'balance': instance.balance,
       'balance_eth': instance.balance_eth,
+      'is_active': instance.is_active,
+      'transactions': instance.transactions,
     };
